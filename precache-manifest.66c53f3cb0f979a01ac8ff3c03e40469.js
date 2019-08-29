1e132d860ac4026749cd022eaefcc33a82fbdacb
stojan-gj <stojan.gjeorgiov@gmail.com>
2019-08-29 12:31:54 +0200

    Updates

---
+++ precache-manifest.66c53f3cb0f979a01ac8ff3c03e40469.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d98c671d912563c8f8672bcf9e9d59f",
+    "url": "/sg-portfolio/index.html"
+  },
+  {
+    "revision": "91f640ca4b4e0b38c537",
+    "url": "/sg-portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "bdfe06e8ea48e02cfd09",
+    "url": "/sg-portfolio/static/css/main.974351d2.chunk.css"
+  },
+  {
+    "revision": "91f640ca4b4e0b38c537",
+    "url": "/sg-portfolio/static/js/2.b18e2328.chunk.js"
+  },
+  {
+    "revision": "bdfe06e8ea48e02cfd09",
+    "url": "/sg-portfolio/static/js/main.80e66425.chunk.js"
+  },
+  {
+    "revision": "c4165e3d1b95666b6578",
+    "url": "/sg-portfolio/static/js/runtime~main.118ccbb6.js"
+  }
+]);
