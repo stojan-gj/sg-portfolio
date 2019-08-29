3751d7ee7193f442bb8fc37e6ec1eb4dc3496071
stojan-gj <stojan.gjeorgiov@gmail.com>
2019-08-29 13:43:18 +0200

    change landing path

--- src/components/main.js
+++ src/components/main.js
@@ -7,8 +7,8 @@ import Contact from "./contact";
 
 const Main = () => (
   <Switch>
-    <Route exact path="/sg-portfolio" component={Landing} />
-    <Route path="/" component={Landing} />
+    <Route exact path="/" component={Landing} />
+    <Route path="/sg-portfolio" component={Landing} />
     <Route path="/resume" component={Resume} />
     <Route path="/projects" component={Projects} />
     <Route path="/contact" component={Contact} />
