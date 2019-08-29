80c3b814f10e646ffc5355f00314bb67658ce55f
stojan-gj <stojan.gjeorgiov@gmail.com>
2019-08-29 13:00:29 +0200

    commit path landing

--- src/components/main.js
+++ src/components/main.js
@@ -7,7 +7,7 @@ import Contact from "./contact";
 
 const Main = () => (
   <Switch>
-    <Route exact path="/" component={Landing} />
+    <Route exact path="/sg-portfolio/" component={Landing} />
     <Route path="/resume" component={Resume} />
     <Route path="/projects" component={Projects} />
     <Route path="/contact" component={Contact} />
